6288e0cf82c9d4b8442cccbc3554ac3620072561
spacedawwwg <spacedawwwg@gmail.com>
2023-04-27 11:21:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-wfmvrp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-wfmvrp)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+defineProps<{ msg: string }>();
+
+const emits = defineEmits<{
+  (eventName: 'foo', value: number | undefined): void;
+}>();
+
+function handleFoo(bar?: number) {
+  console.log('bar', bar);
+}
+
+function handleClick() {
+  emit('foo');
+}
+</script>
+
+<template>
+  <p>{{ msg }}</p>
+  <button @click="handleClick">Click me</button>
+  <hr />
+  <HelloWorld @foo="handleRandom" />
+</template>
